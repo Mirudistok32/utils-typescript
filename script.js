6a84c7592c5fc068828e0ed93777a593869b0fe0
Mirudistok <mirudistok32@yandex.ru>
2020-09-14 13:14:36 +0300

    Required

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 "use strict";
+// //Partial
+// type Person1 = { name: string, age: number }
+// type Person2 = Partial<Person1> // makes everything fields required
+// // keyof - get the keys
+// // next
+// // we create a new keys "P"
+// type MyPartial<T> = { [P in keyof T]?: T[P] } // keyof Person1 // Person[P]
+// type Person3 = MyPartial<Person1>
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,9 +1,15 @@
 
-//Partial
-type Person1 = { name: string, age: number }
-type Person2 = Partial<Person1> // makes everything fields required
-// keyof - get the keys
-// next
-// we create a new keys "P"
-type MyPartial<T> = { [P in keyof T]?: T[P] } // keyof Person1 // Person[P]
-type Person3 = MyPartial<Person1>
+// //Partial
+// type Person1 = { name: string, age: number }
+// type Person2 = Partial<Person1> // makes everything fields required
+// // keyof - get the keys
+// // next
+// // we create a new keys "P"
+// type MyPartial<T> = { [P in keyof T]?: T[P] } // keyof Person1 // Person[P]
+// type Person3 = MyPartial<Person1>
+
+// Required
+type Animal1 = {name?:string, weight?: number}
+type Animal2 = Required<Animal1>
+type MyRequired<T> = {[P in keyof T]-?: T[P]}
+type Animal3 = MyRequired<Animal1> 
